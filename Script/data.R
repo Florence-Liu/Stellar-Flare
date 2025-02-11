352dea2aa0be6f0ba8eaee20ef51809ffb2fc5ef
Florence-Liu <vanccino@gmail.com>
2025-02-11 15:53:55 -0500

    Update saved flux data

--- Script/data.R
+++ Script/data.R
@@ -1,4 +1,4 @@
-data_013 <- read.csv("~/Stellar-Flare/Input/Data/0131799991.csv")
+data_013 <- read.csv("./Input/Data/0131799991.csv")
 data_129 <- read.csv("./Input/Data/129646813.csv")
 data_031 <- read.csv("./Input/Data/031381302.csv")
 summary(data_013);summary(data_129);summary(data_031)
@@ -11,6 +11,6 @@ data031_flux <- data_031 |> select(time, pdcsap_flux)
 summary(data013_flux);summary(data129_flux);summary(data031_flux)
 
 
-write.csv(data013_flux, "./Output/Data013179991_flux_only", row.names = F)
-write.csv(data129_flux, "./Output/Data129646813_flux_only", row.names = F)
-write.csv(data031_flux, "./Output/Data031381302_flux_only", row.names = F)
+write.csv(data013_flux, "./Output/Data/013179991_flux_only", row.names = F)
+write.csv(data129_flux, "./Output/Data/129646813_flux_only", row.names = F)
+write.csv(data031_flux, "./Output/Data/031381302_flux_only", row.names = F)
