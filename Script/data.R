87e324a1475c0b98531164014b287ebc04481021
Florence-Liu <vanccino@gmail.com>
2025-02-11 15:52:46 -0500

    Save updated data

--- Script/data.R
+++ Script/data.R
@@ -1,17 +1,16 @@
-setwd("/Users/florenceliu/Downloads/uoft/MSc/2025 winter/sta2453/stellar flare")
-data_013 <- read.csv("0131799991.csv")
-data_129 <- read.csv("129646813.csv")
-data_031 <- read.csv("031381302.csv")
+data_013 <- read.csv("~/Stellar-Flare/Input/Data/0131799991.csv")
+data_129 <- read.csv("./Input/Data/129646813.csv")
+data_031 <- read.csv("./Input/Data/031381302.csv")
 summary(data_013);summary(data_129);summary(data_031)
+
 library(tidyverse)
+
 data013_flux <- data_013 |> select(time, pdcsap_flux)
 data129_flux <- data_129 |> select(time, pdcsap_flux)
 data031_flux <- data_031 |> select(time, pdcsap_flux)
 summary(data013_flux);summary(data129_flux);summary(data031_flux)
 
-plot(data013_flux$time, data013_flux$pdcsap_flux, type = "l", col = "forestgreen",
-     xlab = "Time", ylab = "Flux", main = "TIC 0131799991")
-plot(data129_flux$time, data129_flux$pdcsap_flux, type = "l", col = "goldenrod1",
-     xlab = "Time", ylab = "Flux", main = "TIC 129646813")
-plot(data031_flux$time, data031_flux$pdcsap_flux, type = "l", col = "lightsteelblue2",
-     xlab = "Time", ylab = "Flux", main = "TIC 031381302")
+
+write.csv(data013_flux, "./Output/Data013179991_flux_only", row.names = F)
+write.csv(data129_flux, "./Output/Data129646813_flux_only", row.names = F)
+write.csv(data031_flux, "./Output/Data031381302_flux_only", row.names = F)
