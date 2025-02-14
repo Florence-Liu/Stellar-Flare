63c2dce816b3093e7d14e4acab6cb32b85e64ab5
Florence-Liu <vanccino@gmail.com>
2025-02-14 17:55:46 -0500

    Change data file style

--- Script/data.R
+++ Script/data.R
@@ -10,7 +10,7 @@ data129_flux <- data_129 |> select(time, pdcsap_flux)
 data031_flux <- data_031 |> select(time, pdcsap_flux)
 summary(data013_flux);summary(data129_flux);summary(data031_flux)
 
-
-# write.csv(data013_flux, "./Output/Data/013179991_flux_only", row.names = F)
-# write.csv(data129_flux, "./Output/Data/129646813_flux_only", row.names = F)
-# write.csv(data031_flux, "./Output/Data/031381302_flux_only", row.names = F)
+# 
+# write.csv(data013_flux, "./Output/Data/013179991_flux_only.csv", row.names = F)
+# write.csv(data129_flux, "./Output/Data/129646813_flux_only.csv", row.names = F)
+# write.csv(data031_flux, "./Output/Data/031381302_flux_only.csv", row.names = F)
