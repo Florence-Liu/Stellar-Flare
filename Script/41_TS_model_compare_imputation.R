1363843e8fc4fef0048f66fe89182d3896def9a5
Florence-Liu <vanccino@gmail.com>
2025-03-09 01:42:28 -0500

    Update analysis for imputation

--- Script/41_TS_model_compare_imputation.R
+++
@@ -1,7 +0,0 @@
-#### Preamble ####
-# Purpose: Comparing before imputation and after imputation models on 
-# Light Curve data for TIC 129646813.
-
-#### Workspace setup ####
-library(tidyverse)
-library(ggplot2)
