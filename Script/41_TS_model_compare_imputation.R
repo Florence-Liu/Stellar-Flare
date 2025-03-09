8e492f8e4784e0a27ed4f70ba0539fb5ff0046e9
Florence-Liu <vanccino@gmail.com>
2025-03-08 21:38:28 -0500

    Update sensitivity analysis for imputation

---
+++ Script/41_TS_model_compare_imputation.R
@@ -0,0 +1,7 @@
+#### Preamble ####
+# Purpose: Comparing before imputation and after imputation models on 
+# Light Curve data for TIC 129646813.
+
+#### Workspace setup ####
+library(tidyverse)
+library(ggplot2)
